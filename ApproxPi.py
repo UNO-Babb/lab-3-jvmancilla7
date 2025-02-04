98d97ff7ff28fc7c29cee819c554c6ea4a5c0cfd
jvmancilla7 <jmancilla2@unl.edu>
2025-02-04 02:08:08 +0000

    DONE WITH PIE

--- ApproxPi.py
+++ ApproxPi.py
@@ -1,7 +1,7 @@
 #ApproxPi.py
-#Name:
-#Date:
-#Assignment:
+#Name: Juan V Mancilla Vargas
+#Date:2/3/2025
+#Assignment:ApproxPi
 import math
 import time
 
@@ -12,10 +12,20 @@ def main():
   realPi = math.pi
 
   #ask user for decimal percision (up to 10)
+  precision= int(input("How many decimal places do you want to approximate pie?"))
 
   start = time.time()
   #calculate pi using the approximation technique
   #Loop until the level of percision is reached
+  approxPi = 4/1
+  sign = -1
+  denom = 3
+  while round(approxPi,precision) != round(realPi,precision):
+    
+    approxPi= approxPi + (sign * 4 / denom)
+
+    sign = sign * -1
+    denom= denom + 2
 
   end = time.time()
 
